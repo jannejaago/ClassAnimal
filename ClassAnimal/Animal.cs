2a8184fce52ab0da32358f2c9153ad9c62efd305
jannejaago <janneaps@gmail.com>
2019-11-18 15:56:31 +0200

    first commit

---
+++ ClassAnimal/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassAnimal
+{
+    class Animal
+    {
+        public string name;
+        public int age = 0;
+        public double weight;
+        public int lifespan = 0;
+        
+        public Animal(string _name, int _age, double _weight, int _lifespan)
+        {
+            name = _name;
+            age = _age;
+            weight = _weight;
+            lifespan = _lifespan;
+        }
+        public void PrintBasicInfo()
+        {
+            Console.WriteLine($"Animal name: {name}.");
+            Console.WriteLine($"Animal age: {age}.");
+            Console.WriteLine($"Animal weight: {weight}.");
+        }
+
+        public void Eat()
+        {
+            weight += 0.2;
+            Console.WriteLine($"{name} is eating.");
+            ShowWeight();
+        }
+
+        public void ShowWeight()
+        {
+            Console.WriteLine($"{name} now weights {weight}.");
+        }
+
+        public void ShowLifespan()
+        {
+            Console.WriteLine($"{name}'s lifespan is {lifespan}.");
+            lifespan++;
+            age++;
+        }
+
+        public void Exercise()
+        {
+            weight -= 0.1;
+            Console.WriteLine($"{name} is exercising.");
+            ShowWeight();
+        }
+    }
+}
---
+++ ClassAnimal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassAnimal
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal("Misty",0,0.3,0);
+
+            for(int i = 0; i<10; i++)
+            {
+                animal.PrintBasicInfo();
+                animal.ShowLifespan();
+                animal.Eat();
+                while (animal.weight > 0.7)
+                {
+                    animal.Exercise();
+                }
+                if (i == 9)
+                {
+                    Console.WriteLine("Your animal dies now because they're old, sorry. Final info:");
+                    animal.PrintBasicInfo();
+                    animal.ShowLifespan();
+                    animal = null;
+                }
+            }
+        }
+    }
+}
